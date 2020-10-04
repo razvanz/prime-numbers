72e83180ae86120247c8a1887cb57b039d037711
Razvan Laurus <razvanlaurus@gmail.com>
2020-10-04 18:38:23 +0200

    feat: hook up HTTP server with gRPC backend

--- src/main/scala/com/example/proxy_server/RouteHandler.scala
+++ src/main/scala/com/example/proxy_server/RouteHandler.scala
@@ -1,11 +1,20 @@
 package com.example.proxy_server
 
 import akka.actor.typed.ActorSystem
-import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
+import akka.grpc.GrpcClientSettings
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.{Route, StandardRoute}
+import akka.util.ByteString
+import com.example.prime_streamer.{ListPrimesRequest, PrimeStreamerClient}
 
 class RouteHandler()(implicit val system: ActorSystem[_]) {
+  val primeStreamerClient: PrimeStreamerClient = PrimeStreamerClient(
+    GrpcClientSettings
+      .connectToServiceAt("localhost", 8081)
+      .withTls(false)
+  )
+
   val routes: Route =
     pathPrefix("primes") {
       path(Segment) { max =>
@@ -18,7 +27,11 @@ class RouteHandler()(implicit val system: ActorSystem[_]) {
       complete(HttpResponse(StatusCodes.BadRequest, entity = "Error: max must be a number >= 2"))
     }
     else {
-      complete("Here go the primes")
+      val byteStringSource = primeStreamerClient.listPrimes(ListPrimesRequest(max.toInt))
+        .map(r => r.number.toString)
+        .map(s => ByteString(s + "\n"))
+
+      complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, byteStringSource))
     }
   }
 }
